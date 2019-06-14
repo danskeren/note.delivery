fe3f4e6568be451ee1737bad1eb60d339a104eb0
danskeren <danskeren@ask.moe>
2019-06-14 15:52:52 +0200

    Added 404 template

---
+++ templates/tmpl/404.tmpl.go
@@ -0,0 +1,18 @@
+package tmpl
+
+const NotFoundHTML = `
+{{ define "content" }}
+<div>
+  <h1>404! The page you're looking for does not exist.</h1>
+  <p>
+    Please double-check that you typed the URL correctly. If you're certain it's the correct URL, then the note has most likely been deleted.
+  </p>
+  <p>
+    Notes created while the 'Allow Deletion' checkbox is enabled can be deleted by anybody who knows the URL.
+  </p>
+  <p>
+    Return to homepage to <a href="/">create a new note</a>.
+  </p>
+</div>
+{{ end }}
+`
