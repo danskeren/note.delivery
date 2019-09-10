8d421581485172495d2e497d12295c4217e0600c
danskeren <danskeren@ask.moe>
2019-09-10 05:42:17 +0200

    Updated textarea content due to linebreak problems on some browsers

--- templates/tmpl/index.tmpl.go
+++ templates/tmpl/index.tmpl.go
@@ -5,13 +5,7 @@ const IndexHTML = `
 
 <form class="content" action="/" method="post">
   <div class="note">
-    <textarea name="note" rows="10" placeholder="
-Be Water, My Friend
-
-[Note.Delivery is open source](https://github.com/danskeren/note.delivery), licensed under AGPLv3.
-
-Let's build a better internet, accessible without JavaScript.
-" required>{{ .NoteContent }}</textarea>
+    <textarea name="note" rows="10" placeholder="Be Water, My Friend" required>{{ .NoteContent }}</textarea>
   </div>
   <div class="password">
     <input name="password" type="password" placeholder="Password (optional)">
